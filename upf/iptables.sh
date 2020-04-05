3d92fdb69fa3fad5829241e9983902ff63b7cb32
lucas baleeiro dominato <lucasdominato2@gmail.com>
2020-04-05 11:55:51 -0300

    I changed iptables to fit my network configurations

--- upf/iptables.sh
+++ upf/iptables.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-iptables -C FORWARD -i uptun -o enp3s0 -j ACCEPT
+iptables -C FORWARD -i uptun -o ens33 -j ACCEPT
 output=$?
 if [ $output -eq 1 ]; then
     echo "Masquerade..."
-    iptables -t nat -A POSTROUTING -o enp3s0 -j MASQUERADE
+    iptables -t nat -A POSTROUTING -o ens33 -j MASQUERADE
     echo "Conntrack..."
     iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
     echo "Uptun..."
-    iptables -A FORWARD -i uptun -o enp3s0 -j ACCEPT
+    iptables -A FORWARD -i uptun -o ens33 -j ACCEPT
 fi
