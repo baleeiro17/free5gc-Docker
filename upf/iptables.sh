7d9cad913f58decfda62084fd44db86c6b788170
lucas baleeiro dominato <lucasdominato2@gmail.com>
2020-04-06 16:47:22 -0300

    update iptables.sh

--- upf/iptables.sh
+++ upf/iptables.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-iptables -C FORWARD -i uptun -o ens33 -j ACCEPT
+iptables -C FORWARD -i uptun -o eno1 -j ACCEPT
 output=$?
 if [ $output -eq 1 ]; then
     echo "Masquerade..."
-    iptables -t nat -A POSTROUTING -o ens33 -j MASQUERADE
+    iptables -t nat -A POSTROUTING -o eno1 -j MASQUERADE
     echo "Conntrack..."
     iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
     echo "Uptun..."
-    iptables -A FORWARD -i uptun -o ens33 -j ACCEPT
+    iptables -A FORWARD -i uptun -o eno1 -j ACCEPT
 fi
